12e2c38d6f42a59c4f35849314e018df57834c83
Elion Limanaj <elionlimanaj@Elions-MacBook-Pro.local>
2020-01-09 09:49:35 -0500

    Links

--- src/com/utils/Constants.java
+++ src/com/utils/Constants.java
@@ -2,8 +2,12 @@ package com.utils;
 
 public class Constants {
 
-	public static final String HRMS_URL="http://166.62.36.207/humanresources/symfony/web/index.php/auth/login";
+	public static final String welcomeToTheInternetUrl = "https://the-internet.herokuapp.com/";
 
-	public static final String SYNTAX_PRACTICE_URL="http://jiravm.centralus.cloudapp.azure.com:8081/index.html";
-	
+	public static final String webOrders = "http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Default.aspx";
+
+	public static final String HRMS_URL = "http://166.62.36.207/humanresources/symfony/web/index.php/auth/login";
+
+	public static final String SYNTAX_PRACTICE_URL = "http://jiravm.centralus.cloudapp.azure.com:8081/index.html";
+
 }
